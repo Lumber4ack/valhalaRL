19e6ee017a0c82b672fd5cf77e03f3812f6af706
BuildTools <unconfigured@null.spigotmc.org>
2022-07-11 21:58:17 +0300

    goblin n trools

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from contextvars import Context
 from html import entities
 import tcod
+import copy
 
 from engine import Engine
 from input_handlers import EventHandler
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -18,6 +19,8 @@ def main() -> None:
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
+
+    max_monsters_per_room = 2
     
     tileset = tcod.tileset.load_tilesheet(
         "arial10x10.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -25,9 +28,7 @@ def main() -> None:
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,255,255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "N", (255,255,0))
-    entities = {npc, player}
+    player = copy.deepcopy(entity_factories.player)
 
     game_map = generate_dungeon(
         max_rooms=max_rooms,
@@ -35,10 +36,11 @@ def main() -> None:
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
+        max_monsters_per_room=max_monsters_per_room,
         player=player
     )
 
-    engine = Engine(entities = entities, event_handler = event_handler, game_map = game_map, player = player)
+    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
