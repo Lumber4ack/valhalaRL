86700b1c70367f8abb0cffa529278a654298b396
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 14:22:53 +0300

    1.0.2 beta chasing enemy

--- entity.py
+++ entity.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 import copy
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from optparse import Option
+from telnetlib import GA
+from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from components.ai import BaseAI
+    from components.fighter import Fighter
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -12,8 +16,12 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
+
+    gamemap: GameMap
+
     def __init__(
         self,
+        gamemap: Optional[GameMap] = None,
         x: int = 0,
         y: int = 0,
         char: str = "?",
@@ -27,6 +35,10 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap:
+            #If gamemap isn't provided now then it will be set later
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy: int) -> None:
         self.x += dx
@@ -37,5 +49,47 @@ class Entity:
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
+        clone.gamemap = gamemap
         gamemap.entities.add(clone)
         return clone
+
+    def place(self, x: int, y: int,gamemap: Optional[GameMap] = None) -> None:
+        """Place this entity at a new location. Handles moving across GameMaps."""
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"):
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
+
+class Actor(Entity):
+    def __init__(
+        self,
+        *, 
+        x: int = 0, 
+        y: int = 0, 
+        char: str = "?", 
+        color: Tuple[int, int, int] = (255, 255, 255), 
+        name: str = "<Unnamed>", 
+        ai_cls: Type[BaseAI],
+        fighter: Fighter
+    ):
+        super().__init__(
+            x=x, 
+            y=y, 
+            char=char, 
+            color=color, 
+            name=name, 
+            blocks_movement=True
+        )
+
+        self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.fighter = fighter
+        self.fighter.entity = self
+
+    @property
+    def is_alive(self) -> bool:
+        """Returns True as long as this actor can perform actions."""
+        return bool(self.ai)
