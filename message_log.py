28181c72d394610cf752e68f1aa2bbe1694e5e90
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 17:27:32 +0300

    1.0.3 message history and hud

---
+++ message_log.py
@@ -0,0 +1,65 @@
+from typing import List, Reversible, Tuple
+import textwrap
+
+import tcod
+import color
+
+
+class Message:
+    def __init__(self, text: str, fg: Tuple[int,int,int]):
+        self.plain_text = text
+        self.fg = fg
+        self.count =1
+
+    @property
+    def full_text(self) -> str:
+        """The full text of this message, including the count if necessary."""
+        if self.count > 1:
+            return f"{self.plain_text} (x{self.count})"
+        return self.plain_text
+
+
+class MessageLog:
+    def __init__(self) -> None:
+        self.messsage: List[Message] = []
+    
+    def add_message(
+        self, text: str, fg: Tuple[int,int,int] = color.white, *, stack: bool = True,
+    ) -> None:
+        """Add a message to this log.
+        `text` is the message text, `fg` is the thext color.
+        If `stack` is True then the message can stack with a previus message
+        of the same text."""
+        if stack and self.messsage and text == self.messsage[-1].plain_text:
+            self.messsage[-1].count += 1
+        else:
+            self.messsage.append(Message(text, fg))
+
+    def render(
+        self, console: tcod.Console, x: int, y: int, width: int, height: int,
+    ) -> None:
+        """Render this log over the given area.
+        `x`, `y`, `width`, `heigth` is the rectangular region to render onto
+        the `console`."""
+        self.render_messages(console, x, y, width, height, self.messsage)
+    
+    @staticmethod
+    def render_messages(
+        console: tcod.Console,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        message: Reversible[Message],
+    ) -> None:
+        """Render the messages provided.
+        The `messages` are rendered starting at the last  message and working
+        backwards."""
+        y_offset = height - 1
+
+        for message in reversed(message):
+            for line in reversed(textwrap.wrap(message.full_text, width)):
+                console.print(x=x, y=y + y_offset, string=line, fg= message.fg)
+                y_offset -= 1
+                if y_offset < 0:
+                    return
